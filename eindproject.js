0feafa7ab709783e5143bc7a50e899177bf34359
Liam <37994@ma-web.nl>
2023-10-16 14:48:54 +0200

    update

---
+++ eindproject.js
@@ -0,0 +1,23 @@
+const rlc = require("readline-sync")
+
+var vraag1 = rlc.question('gmmgxhchjm');
+if (vraag1 == "a") {
+    console.log(vraag2)
+}
+else if (vraag1 == "b") {
+    console.log(vraag3)
+}
+var vraag2 = rlc.question('gchjgcjhcgjxcg');
+if (vraag1 == 'a') {
+    console.log(vraag2)
+}
+else if (vraag1 == 'b') {
+    console.log(vraag3)
+}
+var vraag3 = rlc.question('asd');
+if (vraag1 == 'a') {
+    console.log(vraag2)
+}
+else if (vraag1 == 'b') {
+    console.log(vraag3)
+}
